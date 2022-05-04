333076a2c67a6cb1273f62f25f56a75965685dde
-l <dbonineiv@gmail.com>
2022-05-04 15:34:48 -0700

    Completed page show/hide behaviours

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,83 @@
-// Hide pages on load
 var questions = document.querySelectorAll(".questionPage");
 var startpage = document.querySelector("#startpage");
 var scorepage = document.querySelector("#scorePage");
 var highscores = document.querySelector("#highscores");
-var q1 = document.querySelector("#Page1");
-var q2 = document.querySelector("#Page2");
-var q3 = document.querySelector("#Page3");
-var q4 = document.querySelector("#Page4");
-var q5 = document.querySelector("#Page5");
 var startbtn = document.querySelector("#startbutton");
-viewscores = document.querySelector("#viewhighscores");
+var answers = document.querySelectorAll(".answer");
+var section = document.querySelector("section");
+var viewscores = document.querySelector("#viewhighscores");
+var homebtn = document.querySelector("#homebtn");
+var currentpage = 0;
 
-console.log(document.querySelectorAll(".hidden"));
+console.log(questions.length);
 startbtn.addEventListener("click", StartQuiz);
 viewscores.addEventListener("click", DisplayScores);
+homebtn.addEventListener("click", ReturnHome);
 
-function DisplayScores(event) {
-  event.preventDefault();
-  for (let index = 0; index < questions.length; index++) {
-    questions[index].classList.add("hidden");
-  }
-  startpage.classList.add("hidden");
-  highscores.classList.remove("hidden");
+// Countdown Timer
+
+// Event listener Answer buttons
+for (let i = 0; i < answers.length; i++) {
+  answers[i].addEventListener("click", AnswerClick);
 }
 
+// Back to Start Page
+function ReturnHome() {
+  startpage.classList.remove("hidden");
+  questions[currentpage].classList.add("hidden");
+  highscores.classList.add("hidden");
+  scorepage.classList.add("hidden");
+}
+
+// Show High Score page
+function DisplayScores() {
+  questions[currentpage].classList.add("hidden");
+  startpage.classList.add("hidden");
+  highscores.classList.remove("hidden");
+  scorepage.classList.add("hidden");
+}
+
+// Start quiz
 function StartQuiz(event) {
   event.preventDefault();
   startpage.classList.add("hidden");
-  q1.classList.remove("hidden");
+  questions[currentpage].classList.remove("hidden");
+  console.log(currentpage);
 }
+
+// Show next page
+function AnswerClick(event) {
+  event.preventDefault();
+  console.log(event.target);
+  // Correct answer behaviour
+  if (event.target.classList.contains("correct")) {
+    console.log("correct");
+    // Display "Correct!"
+    // Wrong answer behaviour
+  } else {
+    console.log("Wrong");
+    //display "Wrong!"
+  }
+
+  if (currentpage < questions.length - 1) {
+    currentpage++;
+    questions[currentpage - 1].classList.add("hidden");
+    console.log(currentpage);
+
+    questions[currentpage].classList.remove("hidden");
+  } else {
+    questions[currentpage].classList.add("hidden");
+    EndQuiz();
+  }
+}
+
+// End quiz, display score.
+function EndQuiz() {
+  //  StopTime;
+  scorepage.classList.remove("hidden");
+}
+
 // Create coundown timer (60seconds), start on button press, show end page when timer hits 0
-// on click "view highscores" display high score page
-// On start, display random quiz page
-// on button click, display random quiz page
 // on right answer, display "correct" for 1 second
 // on wrong answer, subrtract time from clock, display "wrong" for 1 second
 // after 5 questions, stop timer, show score page. save time remaining to local storage.
